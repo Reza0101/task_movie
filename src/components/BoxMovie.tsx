d3a4ece719b4d11420a24618c9e5dba1e2d5ca60
RezaSiahi <rezasiahi8@gmail.com>
2023-11-08 23:09:14 +0330

    add box movie component

--- src/components/BoxMovie.tsx
+++ src/components/BoxMovie.tsx
@@ -1,22 +1,44 @@
 import { Link } from "react-router-dom";
 
-const BoxMovie = () => {
+const BoxMovie = ({
+  runtime,
+  id,
+  year,
+  title,
+  poster,
+}: {
+  runtime: string;
+  id: string;
+  year: string;
+  title: string;
+  poster: string;
+}) => {
   return (
     <div className="rounded-lg relative w-[280px] overflow-hidden shadow-md border border-slate-400">
-      <img src="https://moviesapi.ir/images/tt0103064_screenshot1.jpg" className="w-full h-[219px]" alt="" />
+      <Link to={`/movies/${id}`}>
+        <img
+          src={poster}
+          className="w-full h-[219px] cursor-pointer hover:scale-110 duration-300"
+          alt=""
+        />
+      </Link>
       <p className="text-white font-bold bg-blue-500 p-2 rounded-r-md absolute top-4 left-0">
         HD
       </p>
       <div className="bg-black h-[30px] w-[150px] absolute top-0 -right-10 rotate-45"></div>
       <div className="px-2 my-4">
-        <p className="text-blue-500 font-[700] text-[17px]">The Last Of Us</p>
+        <p className="text-blue-500 font-[700] text-[17px] overflow-hidden h-[50px]">{title}</p>
         <div className="flex items-center justify-between">
-            <p>Year: 2022</p>
-            <p>Duration: 75min</p>
+          <p>Year: {year}</p>
+          <p>Duration: {runtime}</p>
         </div>
         <div className="flex justify-end mt-3">
-
-        <Link to='/movies/1' className="text-white bg-blue-500 py-[1px] px-4 rounded-lg">Movie</Link>
+          <Link
+            to={`/movies/${id}`}
+            className="text-white bg-blue-500 py-[1px] px-4 rounded-lg"
+          >
+            Movie
+          </Link>
         </div>
       </div>
     </div>
